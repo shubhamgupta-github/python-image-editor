b452a87e28cc069b5d7bd4b3a7997e195ba2cadd
Shubham Gupta <shubhamgupta083@gmail.com>
2022-12-23 17:38:35 +0530

    source code

---
+++ photoeditor.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image, ImageEnhance, ImageFilter
+
+
+path = "./imgs"  # folder for unedited images
+pathOut = "./editedImgs"  # folder for edited images
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    # sharpening, BW
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+
+    # contrast
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+
+    # ADD MORE EDITS FROM DOCUMENTATION https://pillow.readthedocs.io/en/stable/
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
